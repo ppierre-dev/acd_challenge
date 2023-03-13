9f8439c8bab0d09dced8e2f5eccc9ba3f329ae11
Darunia <maximevaneygen@gmail.com>
2023-03-13 14:44:17 +0100

    profile to showUser

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,10 +7,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-  #[Route('/users', name: 'Users')]
+  #[Route('/users', name: 'users')]
   function index()
   {
     return $this->render('user/index.html.twig', [
+      'controller_name' => 'UserController',
+    ]);
+  }
+
+  #[Route('/users/id', name: 'users_show')]
+  function show()
+  {
+    return $this->render('user/show.html.twig', [
       'controller_name' => 'UserController',
     ]);
   }
