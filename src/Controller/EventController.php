f738ec0d9867be6fc074241f6aefe92b72ba61bf
PaulDlcr <delacrepaul@gmail.com>
2023-03-13 14:33:25 +0100

    event added

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,49 +2,79 @@
 
 namespace App\Controller;
 
-
 use App\Entity\Event;
 use App\Form\EventFormType;
+use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-
+#[Route('/event')]
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    #[Route('/', name: 'app_event_index', methods: ['GET'])]
+    public function index(EventRepository $eventRepository ): Response
     {
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $eventRepository->findAll(),
         ]);
     }
 
-    #[Route('/event/new', name: 'app_event_new')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/new', name: 'app_event_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManage, EventRepository $eventRepository): Response
     {
         $event = new Event();
 
         $form = $this->createForm(EventFormType::class, $event);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $event = $form->getData();
+            $eventRepository->save($event, true);
 
-            $entityManager->persist($event);
-            $entityManager->flush();
-
-            // ... perform some action, such as saving the task to the database
-
-            return $this->redirectToRoute('app_event');
+            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('event/new.html.twig', [
+            'event' => $event,
             'form' => $form,
         ]);
-    
+    }
+
+    #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
+    public function show(Event $event): Response
+    {
+        return $this->render('event/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_event_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        $form = $this->createForm(EventFormType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('event/edit.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_event_delete', methods: ['POST'])]
+    public function delete(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
+            $eventRepository->remove($event, true);
+        }
+
+        return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
     }
 }
+
